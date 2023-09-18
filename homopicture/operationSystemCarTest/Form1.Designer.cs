34c2991f95c7af8aa2f5c002ad6b85e2c2096be9
Vanishshow419 <522250964@qq.com>
2023-09-18 19:33:55 +0800

    first

---
+++ homopicture/operationSystemCarTest/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿using OperationSystemCarTest.Properties;
+
+namespace OperationSystemCarTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.恢复ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.time_rePaint);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "继续脱出";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_mouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.恢复ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(139, 52);
+            // 
+            // 恢复ToolStripMenuItem
+            // 
+            this.恢复ToolStripMenuItem.Name = "恢复ToolStripMenuItem";
+            this.恢复ToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.恢复ToolStripMenuItem.Text = "继续脱出";
+            this.恢复ToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_restore);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.退出ToolStripMenuItem.Text = "退出脱出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_exit);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::OperationSystemCarTest.Properties.Resources.bj;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.No;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1687, 853);
+            this.panel1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::OperationSystemCarTest.Properties.Resources.k3;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(271, 312);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.Location = new System.Drawing.Point(0, 812);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始脱出";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button_start_click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button5.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button5.Location = new System.Drawing.Point(493, 812);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(127, 39);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "索达哟";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_terminate_click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button2.Location = new System.Drawing.Point(116, 812);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "打咩";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_stop_click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button4.Location = new System.Drawing.Point(367, 812);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 39);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "减速脱出";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_decelerate_click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button3.Location = new System.Drawing.Point(241, 812);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "加速脱出";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_speedUp_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1687, 853);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(1709, 908);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "会员制餐厅模拟器（power by ll）";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.form_formClosing);
+            this.Load += new System.EventHandler(this.form_load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.form_mouseClick);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 恢复ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ homopicture/operationSystemCarTest/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Media;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace OperationSystemCarTest
+{
+    public partial class Form1 : Form
+    {
+        private List<Bitmap> images = new List<Bitmap>(); // 用于存储图片的列表
+        private int currentImageIndex = 0; // 当前图片索引
+        // 新增的计时器用于图片切换
+        private Timer imageSwitchTimer = new Timer();
+        private DateTime pauseEndTime;
+        private SoundPlayer soundPlayer;
+        private bool musicPlaying = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            InitializeImages(); // 调用初始化图片的方法
+            // 初始化新计时器
+            imageSwitchTimer.Interval = 100; // 设置间隔为0.1秒
+            imageSwitchTimer.Tick += new EventHandler(imageSwitchTimer_Tick);
+            /*
+             * 以绝对路径导入音乐时使用以下语句(例子)
+             * string musicFilePath = @"C:\Users\user\Desktop\Study\School\Learning\操作系統\實驗\實驗1\operationSystemCarTest\OperationSystemCarTest\Resources\jntm.wav";
+             * soundPlayer = new SoundPlayer(musicFilePath);
+            */
+            soundPlayer = new SoundPlayer(Properties.Resources.homo);
+        }
+
+        private void time_rePaint(object sender, EventArgs e)
+        {
+            //控制坤的移动
+            int move_x = panel1.Size.Width / 162;
+            int move_y = panel1.Size.Height / 192;
+            int car_x = pictureBox1.Location.X + move_x;
+            int car_y = pictureBox1.Location.Y + move_y;
+            //判断坤是否超过窗体区域，超过则重新回到左上角
+            if (car_x > this.Width - pictureBox1.Size.Width || car_y > this.Height - pictureBox1.Size.Height)
+            {
+                car_x = 0;
+                car_y = 0;
+            }
+            //重新设置小坤的位置   
+            pictureBox1.Location = new Point(car_x, car_y);
+        }
+
+        private void InitializeImages()
+        {
+            // 添加你的图片到 images 列表中
+            images.Add(Properties.Resources.k1);
+            images.Add(Properties.Resources.k2);
+            images.Add(Properties.Resources.k3);
+            images.Add(Properties.Resources.k4);
+            images.Add(Properties.Resources.k5);
+            images.Add(Properties.Resources.k6);
+            images.Add(Properties.Resources.k7);
+            images.Add(Properties.Resources.k8);
+            images.Add(Properties.Resources.k3);
+            images.Add(Properties.Resources.k2);
+            images.Add(Properties.Resources.k1);
+
+            // 设置初始图片
+            pictureBox1.Image = images[currentImageIndex];
+        }
+
+        private void imageSwitchTimer_Tick(object sender, EventArgs e)
+        {
+            // 切换到下一张图片
+            if (DateTime.Now >= pauseEndTime)
+            {
+                // 恢复计时器
+                imageSwitchTimer.Start();
+            }
+            currentImageIndex++;
+            if (currentImageIndex >= images.Count)
+            {
+                currentImageIndex = 0; // 循环显示图片
+            }
+
+            pictureBox1.Image = images[currentImageIndex];
+        }
+
+        private void button_start_click(object sender, EventArgs e)
+        {
+            
+            /*
+             * 以绝对路径的方式导入音乐时使用以下语句
+             * soundPlayer.Play();
+            */
+            //音乐播放判断
+            if (!musicPlaying)
+            {
+                soundPlayer.Play();
+                musicPlaying = true;
+            }
+            //开启定时器，开始小坤的移动
+            imageSwitchTimer.Start();
+            timer1.Start();
+        }
+
+        private void button_stop_click(object sender, EventArgs e)
+        {
+            // 计算暂停结束的时间为当前时间加上114月
+            pauseEndTime = DateTime.Now.AddMonths(114);
+            //音乐播放判断
+            if (musicPlaying)
+            {
+                soundPlayer.Stop();
+                musicPlaying = false;
+            }
+            //暂停定时器、音乐播放器，暂停小坤的移动
+            soundPlayer.Stop();
+            imageSwitchTimer.Stop();
+            timer1.Stop();
+        }
+        private void button_speedUp_click(object sender, EventArgs e)
+        {
+            //当刷新的时间间隔大于10ms
+            if (timer1.Interval > 20)
+            {
+                timer1.Interval -= 10;
+            }
+            //当刷新的时间间隔大于5ms小于10ms
+            else if (timer1.Interval > 5)
+            {
+                timer1.Interval -= 1;
+            }
+            else
+            {
+                timer1.Interval = 5;
+            }
+        }
+
+        private void button_decelerate_click(object sender, EventArgs e)
+        {
+            //当刷新的时间间隔小于10ms
+            if (timer1.Interval < 300)
+            {
+                timer1.Interval += 50;
+            }
+            else
+            {
+                timer1.Interval = 350;
+            }
+        }
+
+        private void button_terminate_click(object sender, EventArgs e)
+        {
+            //退出程序
+            System.Environment.Exit(0);
+        }
+
+        private void notifyIcon_mouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //显示本Winform程序
+            this.Show();
+        }
+
+        private void toolStripMenuItem_restore(object sender, EventArgs e)
+        {
+            //显示本WinForm程序
+            this.Show();
+        }
+
+        private void toolStripMenuItem_exit(object sender, EventArgs e)
+        {
+            //退出程序
+            System.Environment.Exit(0);
+        }
+
+        private void form_formClosing(object sender, FormClosingEventArgs e)
+        {
+            //设置点击“X”后不会退出程序
+            e.Cancel = true;
+            //隐藏本WinForm程序
+            this.Hide();
+        }
+
+        private void form_load(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(0, 0);//设置初始位置为最左上角
+        }
+
+        private void form_mouseClick(object sender, MouseEventArgs e)
+        {
+            //显示本WinForm程序
+            this.Show();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 设置图片框的初始位置和大小
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Size = new Size(300, 300); // 根据你的需求设置大小
+        }
+    }
+}
